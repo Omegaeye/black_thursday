cf3a323defcb90b155696cce8ac215c4f7817112
Khoa Nguyen <KhoaNg323@hotmail.com>
2021-01-08 12:28:54 -0700

    Refactoring

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -16,7 +16,7 @@ class MerchantRepositoryTest < Minitest::Test
   def test_it_has_attributes
     assert_instance_of Hash , @dummy_repo.collections
   end
-  
+
   def test_populate_repo
     assert_equal 5, @dummy_repo.collections.count
   end
@@ -26,12 +26,12 @@ class MerchantRepositoryTest < Minitest::Test
   end
 
   def test_find_by_id
-    assert_instance_of Merchant, @dummy_repo.find_by_id("12334141")
-    assert_nil nil, @dummy_repo.find_by_id("12")
+    assert_instance_of Merchant, @dummy_repo.find_by_id(12334141)
+    assert_nil nil, @dummy_repo.find_by_id(12)
   end
 
   def test_find_by_name
-    assert_equal '12334141', @dummy_repo.find_by_name('jejum').id
+    assert_equal 12334141, @dummy_repo.find_by_name('jejum').id
     assert_nil @dummy_repo.find_by_name('Caryn')
   end
 
@@ -43,14 +43,14 @@ class MerchantRepositoryTest < Minitest::Test
   def test_max_id
     assert_equal 12334141, @dummy_repo.max_id
   end
-  
+
   def test_new_id
     assert_equal 12334142, @dummy_repo.new_id
   end
-  
+
   def test_it_can_create
     data ={
-      :id => "910",
+      :id => 910,
       :name => "chipotle",
       :created_at => "2125-09-22 09:34:06 UTC",
       :updated_at => "2034-09-04 21:35:10 UTC"
@@ -58,9 +58,9 @@ class MerchantRepositoryTest < Minitest::Test
 
     @dummy_repo.create(data)
     actual = @dummy_repo.collections.values[-1].id
-    expected = @dummy_repo.max_id 
-    assert_equal "12334142", actual
-  
+    expected = @dummy_repo.max_id
+    assert_equal 12334142, actual
+
     assert_equal expected + 1, actual.to_i+1
   end
 
