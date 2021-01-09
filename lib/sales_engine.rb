66024f57f722fc8f0dfff7fa0ba8251029eda4cd
Khoa Nguyen <KhoaNg323@hotmail.com>
2021-01-09 13:29:09 -0700

    Initialize @invoice_items

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -2,13 +2,14 @@ require_relative './items_repo'
 require_relative './merchant_repository'
 require_relative './analyst'
 require_relative './invoice_repo'
-
+require_relative './invoice_items_repo'
 
 class SalesEngine
   attr_reader :items,
               :merchants,
               :analyst,
-              :invoices
+              :invoices,
+              :invoice_items
 
   def self.from_csv(data)
     new(data)
@@ -19,5 +20,6 @@ class SalesEngine
     @merchants = MerchantRepository.new(data[:merchants], self)
     @analyst = Analyst.new(self)
     @invoices = InvoiceRepo.new(data[:invoices], self)
+    @invoice_items = InvoiceItemRepo.new(data[:invoice_items], self)
   end
 end
